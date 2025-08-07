018103da62d4bc673f99a30229e52939b22fe89c
Vicky5-5 <vicgs99@gmail.com>
2025-08-07 19:25:38 +0200

    Metodos y variables static

--- POO/Empleados/Pruebas.java
+++ POO/Empleados/Pruebas.java
@@ -6,10 +6,17 @@ public class Pruebas {
 		// TODO Auto-generated method stub
 
 		Empleados e1 = new Empleados("Paco");
-		
-		System.out.println(e1.devuelveDatos());
+		Empleados e2 = new Empleados("Ana");
+		Empleados e3 = new Empleados("Roberto");
+
 		e1.cambiarSeccion("Desarrollo");
-		System.out.println(e1.devuelveDatos());
+	
+		e3.cambiarSeccion("Sistemas");
+		
+		System.out.println(e1.devuelveDatos() + "\n" + e2.devuelveDatos() + "\n"+
+		e3.devuelveDatos());
+		System.out.println(Empleados.devuelveIdSiguiente()); //Para llamar al metodo static es nombre de la clase + nombre del método
+	}
 
 		
 	}
@@ -20,17 +27,23 @@ class Empleados {
 
 	private final String nombre; // como no se modifica en todo el proyecto se pone final y no se puede cambiar el valor de nombre
 	private String seccion;
-	
+	private int Id; //Es único para cada empleado
+	private static int IdSiguiente = 1; // esto es una variable static. Siempre tendrá este valor y tiene que estar inicializada.
 	//Estado inicial de cada empleado al entrar 
 	public Empleados(String nom) {
 		nombre = nom;
 		seccion = "Administración";
+		Id=IdSiguiente; //después de decir que el id ess 1 lo incrementamos
+		IdSiguiente++;
 	}
 	
 	public void cambiarSeccion(String seccionNueva) { //Setter
 		this.seccion=seccionNueva;
 	}
-	
+	//los estaticos no trabajan sobre los objetos, lo hacen sobre la clase. Tampoco acceden a los campos ejemplar a no ser que sean static (como IdSiguiente)
+	public static String devuelveIdSiguiente() {
+		return "El ID siguiente es "+ IdSiguiente;
+	}
 	public String devuelveDatos() {
 		return "El nombre es "+ nombre + " y la sección es " + seccion;
 	}
